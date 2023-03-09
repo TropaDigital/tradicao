cfa267208eb44cdd4536f2bb36fd0863f3b05909
Raafa1953 <rafaelajds@gmail.com>
2023-03-09 12:27:17 -0300

    create arq defaults

---
+++ src/app/(page)/contato/head.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Head() {
+  return (
+    <>
+      <title>Contato</title>
+      <meta name="Contact" content="contact" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <Link href="/painel">Ir para Painel</Link>
+      <Link href="/contato">Ir para Contato</Link>
       home
     </div>
   )
