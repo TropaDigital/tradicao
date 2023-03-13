e4358cf2f943a1c7fe7153d34022829927dcfa66
Fino59 <slipdan@hotmail.com>
2023-03-13 18:31:22 -0300

    Update Brands Carousel component

--- src/components/shared/BrandsCarousel/styles.ts
+++ src/components/shared/BrandsCarousel/styles.ts
@@ -5,4 +5,14 @@ export const CarouselContainer = styled.div`
     min-height: 96px;
     max-height: 130px;
     height: fit-content;
+    margin: 50px 0;
+
+    .content {
+        .my-swiper {
+            overflow: hidden;
+            .swiper-wrapper {
+                display: flex;
+            }
+        }
+    }
 `
