94258abbd4c96b8f3e297f96f98123de8165d0c4
Raafa1993 <rafaelajds@gmail.com>
2023-03-13 07:43:24 -0300

    update page

--- src/app/(page)/layout.tsx
+++ src/app/(page)/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
-        <GlobalStyles />
+        {/* <GlobalStyles /> */}
       </StyledJsxRegistry>
     </section>
   );
