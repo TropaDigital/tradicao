efffa4f53662ad5f5d0cbf7bb359f713aa375a52
Fino59 <slipdan@hotmail.com>
2023-03-16 15:04:03 -0300

    Create Pesados page

---
+++ src/app/(page)/pesados/styles.ts
@@ -0,0 +1,17 @@
+'use client'
+import styled from "styled-components";
+
+export const ContainerTop = styled.div`
+    height: 500px;
+    position: relative;
+
+    .margin {
+        margin-top: 30px;
+    }
+`
+
+export const SubtitleTop = styled.div`
+  font-size: var(--text-smal-xl);
+  font-weight: var(--weight-bold);
+  color: var(--gray-600);
+`
