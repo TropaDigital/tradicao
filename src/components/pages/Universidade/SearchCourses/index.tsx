fa559fa15962b6044aa1cf61dfd6e8c062b903e6
Fino59 <slipdan@hotmail.com>
2023-03-17 15:15:00 -0300

    Update Search Courses component

--- src/components/pages/Universidade/SearchCourses/index.tsx
+++ src/components/pages/Universidade/SearchCourses/index.tsx
@@ -1,10 +1,34 @@
 'use client';
+import { BrushedIcon, CircleLogoIcon } from "@/assets/icons";
 import Button from "@/components/UI/Button";
 import { InputDefault } from "@/components/UI/Inputs/InputDefault";
+
 // Styles
-import { CourseCard, CourseCardBottom, CourseCardTop, CourseCardWrapper, SearchContainer, SearchHeader } from "./styles";
+import { 
+    CourseCard, 
+    CourseCardBottom, 
+    CourseCardTop, 
+    CourseCardWrapper, 
+    SearchContainer, 
+    SearchHeader 
+} from "./styles";
+
+interface ICoursesProps {
+    id: number,
+    bitTitle: string,
+    description: string,
+}
 
 export default function SearchCourses() {
+
+    const Courses: ICoursesProps[] = [
+        {
+            id: 1,
+            bitTitle: 'Testes',
+            description: 'sei lá.'
+        }
+    ]
+
     return (
         <SearchContainer>
             <SearchHeader>
@@ -20,7 +44,12 @@ export default function SearchCourses() {
                 <CourseCard>
                     <CourseCardTop>
                         Integração Consórcio Tradição
-                        
+                        <div className="icon-bottom">
+                            <div className="brushed">
+                                <BrushedIcon />
+                            </div>
+                            <CircleLogoIcon />
+                        </div>
                     </CourseCardTop>
                     <CourseCardBottom>
                         Integração Consórcio Tradição com Sheila Cruz
