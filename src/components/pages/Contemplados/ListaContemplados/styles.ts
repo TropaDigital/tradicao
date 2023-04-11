9090915e89404fd5f1ac3116a0bef40a73115101
Fino59 <slipdan@hotmail.com>
2023-04-11 18:38:16 -0300

    Create Contemplados page component modal

---
+++ src/components/pages/Contemplados/ListaContemplados/styles.ts
@@ -0,0 +1,69 @@
+'use client';
+import styled from "styled-components";
+
+export const ModalWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+`
+
+export const ModalTitle = styled.div`
+    font-size: 32px;
+    font-weight: 700;
+    color: var(--primary);
+`
+
+export const Table = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    thead,
+    tbody {
+        tr {
+            display: grid;
+            grid-template-columns: repeat(5, 1fr);
+            gap: 16px;
+        }
+    }
+
+    thead {
+        tr {
+            height: fit-content;
+            margin-bottom: 6px;
+        }
+    }
+
+    tbody {
+        tr {
+            &:nth-child(odd) {
+                background: var(--gray-200);
+            }
+        }             
+    }
+`
+
+export const TdTitle = styled.div`
+    height: fit-content;
+
+    font-size: 18px;
+    font-weight: 600;
+    color: var(--primary-dark);
+
+    padding: 8px 0;
+
+    &.first {
+        padding-left: 8px;
+    }
+`
+
+export const TdInfo = styled.div`
+    height: fit-content;
+
+    color: var(--gray-900);
+
+    padding: 8px 0;
+
+    &.first {
+        padding-left: 8px;
+    }
+` 
