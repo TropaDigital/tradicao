7b426dd07c8526ffbf975256d25b57435d41e6e0
cleibson-nenesio <cleibson.nenesio@hotmail.com>
2023-04-11 11:04:44 -0300

    update baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV === 'development') {
   // baseUrl = 'http://192.168.15.12:3000/';
   baseUrl = 'http://192.168.15.15:33333';
 } else {
-  // baseUrl = 'https://tradicao.backendtropa.com.br/';
-  baseUrl = 'http://192.168.15.15:33333';
+  baseUrl = 'https://tradicao.backendtropa.com.br/';
+  // baseUrl = 'http://192.168.15.15:33333';
 }
 
 const API = axios.create({
