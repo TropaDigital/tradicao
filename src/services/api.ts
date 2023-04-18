6fadd45a707e5f9aee259b9a112ca850643d6c2f
Fino59 <slipdan@hotmail.com>
2023-04-18 10:52:55 -0300

    Update local connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ var baseUrl;
 if (process.env.NODE_ENV === 'development') {
   // baseUrl = 'https://tradicao.backendtropa.com.br/';
   // baseUrl = 'http://192.168.15.12:3000/';
-  baseUrl = 'http://192.168.15.15:33333/';
+  baseUrl = 'http://192.168.15.14:33333/';
 } else {
   // baseUrl = 'https://tradicao.backendtropa.com.br/';
   baseUrl = 'http://192.168.15.15:33333';
