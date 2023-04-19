5381f3a307d8daa796ee6c02fe64205936785511
Felipe Sanfilippo <feliperodrigues.san@gmail.com>
2023-04-19 11:10:47 -0300

    att - contador home

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
       <Speciality />
 
-      <BlueInfos quotasTotal="39.583" creditsTotal="501.260.000" />
+      <BlueInfos quotasTotal="39583" creditsTotal="501260000" />
 
       <ConsortiumWorks />
 
--- src/components/pages/Home/BlueInfos/index.tsx
+++ src/components/pages/Home/BlueInfos/index.tsx
@@ -1,29 +1,84 @@
-import { BlueContainer, BlueLeft, BlueLeftInfo, BlueRight, BlueRightInfo } from "./styles";
+import { useEffect, useState } from 'react';
+import {
+  BlueContainer,
+  BlueLeft,
+  BlueLeftInfo,
+  BlueRight,
+  BlueRightInfo
+} from './styles';
+
+import eases from 'eases';
 
 interface IBlueInfoProps {
-    quotasTotal: string,
-    creditsTotal: string,
+  quotasTotal: string;
+  creditsTotal: string;
 }
 
+export default function BlueInfos({
+  quotasTotal,
+  creditsTotal
+}: IBlueInfoProps) {
 
-export default function BlueInfos({quotasTotal, creditsTotal}: IBlueInfoProps) {
-    return (
-        <BlueContainer>
-            
-            <BlueLeft>                
-                <BlueLeftInfo>
-                    +{quotasTotal}
-                    <div className="title">cotas contempladas</div>
-                </BlueLeftInfo>
-            </BlueLeft>
+  const numero = parseInt(quotasTotal);
+  const numero2 = parseInt(creditsTotal);
 
-            <BlueRight>
-                <BlueRightInfo>
-                    +<span>R$</span>{creditsTotal}
-                    <div className="title">em créditos</div>
-                </BlueRightInfo>
-            </BlueRight>
-        
-        </BlueContainer>
-    )
+  const [valorCotas, setValorCotas] = useState(0);
+  const [valorCredito, setValorCredito] = useState(0);
+  const duracao = numero > 50 ? 2000 * (50 / numero) : 2000;
+  const duracao2 = numero2 > 50 ? 2000 * (50 / numero2) : 2000;
+  const intervalo = duracao / numero;
+  const intervalo2 = duracao / numero2;
+
+  useEffect(() => {
+    let contador = 0;
+    const timer = setInterval(() => {
+      contador += 100
+      const tempo = contador * intervalo;
+      const progresso = tempo / duracao;
+      const interpolacao = eases.circInOut(progresso);
+      const valor = Math.round(numero * interpolacao);
+      setValorCotas(valor);
+      if (contador >= duracao / intervalo) {
+        clearInterval(timer);
+        setValorCotas(numero);
+      }
+    }, intervalo);
+    return () => clearInterval(timer);
+  }, [numero, duracao, intervalo]);
+
+  useEffect(() => {
+    let contador = 0;
+    const timer = setInterval(() => {
+      contador+= 88
+      const tempo = contador * intervalo2;
+      const progresso = tempo / duracao2;
+      const interpolacao = eases.circInOut(progresso);
+      const valor = Math.round(numero2 * interpolacao);
+      setValorCredito(valor);
+      if (contador >= duracao2 / intervalo2) {
+        clearInterval(timer);
+        setValorCredito(numero2);
+      }
+    }, intervalo2);
+    return () => clearInterval(timer);
+  }, [numero2, duracao2, intervalo2]);
+
+  return (
+    <BlueContainer>
+      <BlueLeft>
+        <BlueLeftInfo>
+          +{valorCotas}
+          <div className="title">cotas contempladas</div>
+        </BlueLeftInfo>
+      </BlueLeft>
+
+      <BlueRight>
+        <BlueRightInfo>
+          +<span>R$</span>
+          {valorCredito}
+          <div className="title">em créditos</div>
+        </BlueRightInfo>
+      </BlueRight>
+    </BlueContainer>
+  );
 }
