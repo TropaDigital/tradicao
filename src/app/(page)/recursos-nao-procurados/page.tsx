5988645888fc77d4b55f9afe0d4c4aa0220eac69
Fino59 <slipdan@hotmail.com>
2023-04-19 10:46:44 -0300

    Update Recursos Não Procurados page

--- src/app/(page)/recursos-nao-procurados/page.tsx
+++ src/app/(page)/recursos-nao-procurados/page.tsx
@@ -27,72 +27,11 @@ interface IReceiveProps {
     value: string,
 }
 
-
 export default function NotReceived() {
     const [ cpfSearch, setCpfSearch ] = useState<string>('');
     const [ startSearch, setStartSearch ] = useState<any>('');
 
-    const listReceive: IReceiveProps[] = [
-        {
-            id: 0,
-            cpf: '999.999.999-99',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 1,
-            cpf: '888.888.888-88',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 2,
-            cpf: '777.777.777-77',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 3,
-            cpf: '666.666.666-66',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 4,
-            cpf: '555.555.555-55',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 5,
-            cpf: '444.444.444-44',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 6,
-            cpf: '333.333.333-33',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 7,
-            cpf: '222.222.222-22',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 8,
-            cpf: '111.111.111-11',
-            value: 'R$ 375,00'
-        },
-        {
-            id: 9,
-            cpf: '000.000.000-00',
-            value: 'R$ 375,00'
-        },
-    ]
-
-    const { allRecursos } = useGetAllRecursos('');
-
-    const filteredCpfs = useMemo(() => {
-        const searchLowercase = cpfSearch?.toLowerCase();
-
-        return allRecursos?.result.filter((obj) => obj.cpf.toLowerCase().includes(searchLowercase))
-
-    }, [startSearch]);
+    const { getRecursos } = useGetAllRecursos(`?pesquisa=${startSearch}`);    
 
     return (
         <>
@@ -146,7 +85,7 @@ export default function NotReceived() {
                             className="button-search"
                             type="button"
                             disabled={!cpfSearch}
-                            onClick={() => {setStartSearch(new Date()); console.log('log do disabled')}}
+                            onClick={() => setStartSearch(cpfSearch)}
                         >
                             Buscar
                         </Button>
@@ -160,13 +99,9 @@ export default function NotReceived() {
                         {
                             cpfSearch &&
                             startSearch &&
-                            filteredCpfs.length > 0 
-                            ?
-                                filteredCpfs.map((row: any) => (
-                                    <div className="valid" key={row.id}>Você tem recursos a receber!</div>
-                                ))                       
-                            :   <div className="invalid">Você não tem recurso a receber!</div>                       
-                                
+                            getRecursos.result.length > 0
+                            ?   <div className="valid">Você tem recursos a receber!</div>                                
+                            :   <div className="invalid">Você não tem recurso a receber!</div> 
                         }            
                     </S.ContainerResult>
                 }
