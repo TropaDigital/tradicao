051655f2c63caf040debeecb2d42aaadd2cd4894
Fino59 <slipdan@hotmail.com>
2023-04-20 12:39:29 -0300

    Update Recursos Não Procurados painel with search field

--- src/components/pages/Universidade/UniversityLogin/yupSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const universityYupSchema = yup.object().shape({
-  user: yup.string().required('Nome é obrigatório'),
-  password: yup.string().required('Senha é obrigatória'),  
-});
