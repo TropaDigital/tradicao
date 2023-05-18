439022591ec324f8f473ebc3aa68e5531278a61b
Cleibson <cleibson.nenesio@hotmail.com>
2023-05-18 09:23:37 -0300

    feat: Adicionado scripts google & rdstations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,25 @@ export default function RootLayout({
           src="//titulares.becompliance.com/becompliance.js"
           defer
         ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3LNMB2ZH0R"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-3LNMB2ZH0R');
+              `
+          }}
+        ></script>
+        <script
+          type="text/javascript"
+          src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/62afde1d-bc84-4c9b-a382-a60fea3f34a4-loader.js"
+          async
+        ></script>
       </body>
     </html>
   );
