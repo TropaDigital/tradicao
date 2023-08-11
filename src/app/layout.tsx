681255d00d72c4bd22e1a283420545434eadd2ae
Cleibson <cleibson.nenesio@hotmail.com>
2023-08-11 15:56:43 -0300

    fix: Deixada somente 1 tag de analytics 'GTM-PH24RMN'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,15 @@ export default function RootLayout({
     <html lang="pt-BR">
       <Head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PH25RMN');`
+          }}
+        />
+        <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=G-3LNMB2ZH0R`}
         ></script>
@@ -50,24 +59,6 @@ export default function RootLayout({
         />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5R8S5VF');`
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PH25RMN');`
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -79,7 +70,10 @@ export default function RootLayout({
       <body>
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PH25RMN" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PH25RMN"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `
           }}
         />
         <QueryClientProvider client={queryClient}>
@@ -98,10 +92,6 @@ export default function RootLayout({
           </StyledJsxRegistry>
         </QueryClientProvider>
 
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3LNMB2ZH0R"
-        ></script> */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-X3SM6Q273G"
